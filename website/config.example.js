f6195aa2b42fd919f9a99262426acbc85b6500de
dylan-almonte <dcabahug@andrew.cmu.edu>
2026-02-07 12:23:34 -0500

    login

---
+++ website/config.example.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  EXTENSION_ID: "your-chrome-extension-id-here",
+  NESSIE_API_KEY: "your-nessie-api-key-here",
+  NESSIE_BASE_URLS: [
+    "http://api.nessieisreal.com",
+    "http://api.reimaginebanking.com",
+    "https://api.nessieisreal.com",
+    "https://api.reimaginebanking.com",
+  ],
+};
